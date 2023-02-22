a3f9413a984ddc5b6be4ea2e40022ec12b5c89ae
nadav <thekingthekongshalom@gmail.com>
2023-02-22 18:40:45 +0200

    import calc_robot_pos

--- project/src/main/java/frc/robot/Robot.java
+++ project/src/main/java/frc/robot/Robot.java
@@ -1,12 +1,18 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.math.kinematics.MecanumDriveOdometry;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.TankDrive;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,6 +25,14 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+  private long m_time;
+  private double m_deltaTime; 
+  private CANSparkMax m_frontRightWheel = new CANSparkMax(Constants.Ports.RIGHT_FRONT_PORT, MotorType.kBrushless);
+  private CANSparkMax m_frontLeftWheel  = new CANSparkMax(Constants.Ports.LEFT_FRONT_PORT,  MotorType.kBrushless);
+  private AHRS m_navx = new AHRS(SPI.Port.kMXP);
+  private double m_xPos = 0;
+  private double m_yPos = 0;
+  
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -28,6 +42,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    m_time = System.currentTimeMillis();
   }
 
   /**
@@ -44,6 +59,34 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+  
+    m_deltaTime = System.currentTimeMillis() / 1000.0 - m_time / 1000.0;
+
+    double leftSideVelocity = Constants.Conversions.rpm2ms(Constants.Values.TANKDRIVE_WHEEL_RADIUS, m_frontLeftWheel.getEncoder().getVelocity());
+    double rightSideVelocity = Constants.Conversions.rpm2ms(Constants.Values.TANKDRIVE_WHEEL_RADIUS, m_frontRightWheel.getEncoder().getVelocity());
+
+    double leftSideDistance = leftSideVelocity * m_deltaTime;
+    double rightSideDistance = rightSideVelocity * m_deltaTime; 
+   
+    double leftSideAngle = leftSideDistance / Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT;
+    double rightSideAngle = rightSideDistance / Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT;
+    
+    double leftSideX  = Math.cos(leftSideAngle) * Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT;
+    double leftSideY  = Math.sin(leftSideAngle) * Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT;
+    
+    double rightSideX = Math.cos(rightSideAngle) * Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT;
+    double rightSideY = Math.sin(rightSideAngle) * Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT;
+
+    double deltaX = leftSideX + rightSideX;
+    double deltaY = leftSideY + rightSideY;
+
+    m_xPos += deltaX;
+    m_yPos += deltaY;
+
+    SmartDashboard.putNumber("x", m_xPos);
+    SmartDashboard.putNumber("y", m_yPos);
+    m_time = System.currentTimeMillis();
+
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
@@ -91,7 +134,11 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+
+    RobotContainer.m_tankDriveCommand.execute();
+    
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
