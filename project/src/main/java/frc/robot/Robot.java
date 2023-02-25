0552e8939dcc207c779a44b332e1f37abae1a72b
NegevTag <wildnegev@gmail.com>
2023-02-25 21:15:22 +0200

    MovedPositioncalculation to chassis periodic

--- project/src/main/java/frc/robot/Robot.java
+++ project/src/main/java/frc/robot/Robot.java
@@ -1,18 +1,10 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.kauailabs.navx.frc.AHRS;
-import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj.SPI;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.Chassis;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -28,16 +20,9 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
-  private long m_time;
-  private double m_deltaTime;
-  private double m_lastRightDist;
-  private double m_lastLeftDist;
+  
 
-  private AHRS m_navx = new AHRS(SPI.Port.kMXP);
-  private Vector2D m_pos = new Vector2D(0,0);
-
-  private double m_rightSideDistance = 0;
-  private double m_leftSideDistance = 0;
+  
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -50,7 +35,7 @@ public class Robot extends TimedRobot {
     // and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    m_time = System.currentTimeMillis();
+
   }
 
   /**
@@ -66,43 +51,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
-    SmartDashboard.putNumber("robot speed", 0.4);
-  
-    m_deltaTime = System.currentTimeMillis() / 1000.0 - m_time / 1000.0;
-
-    double deltaRight = Chassis.getInstance().getRightEncoderDist()-m_lastRightDist;
-    m_lastRightDist = Chassis.getInstance().getRightEncoderDist();
-
-    double deltaLeft = Chassis.getInstance().getRightEncoderDist()-m_lastLeftDist;
-    m_lastLeftDist = Chassis.getInstance().getLeftEncoderDist();
-
-    double leftSideAngle = deltaLeft / Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT;
-    double rightSideAngle = deltaRight / Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT;
     
-   Vector2D left = new Vector2D(
-    Math.cos(leftSideAngle) * Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT
-    ,Math.sin(leftSideAngle) * Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT);
-    
-    Vector2D right = new Vector2D(Math.cos(rightSideAngle) * Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT,
-    Math.sin(rightSideAngle) * Constants.Values.DISTANCE_BETWEEN_LEFT_TO_RIGHT);
-    
-    Vector2D robotDelta = right.getAdded(left).getDivided(2);
-    Vector2D fieldDelta = Constants.Conversions.rotateZ(robotDelta,-Chassis.getInstance().getRobotAngle());
-    m_pos.add(fieldDelta);
-    SmartDashboard.putNumber("x", m_pos.x);
-    SmartDashboard.putNumber("y", m_pos.y);
-    // SmartDashboard.putNumber("RPM left velocity",  Chassis.getInstance().getLeftVelocity() / 10.97);
-    // SmartDashboard.putNumber("RPM right velocity", Chassis.getInstance().getRightVelocity() / 10.97);
-    // SmartDashboard.putNumber("ms left velocity", Constants.Conversions.rpm2ms(Constants.Values.TANKDRIVE_WHEEL_RADIUS, Chassis.getInstance().getLeftVelocity()/ 10.97));
-    // SmartDashboard.putNumber("ms right velocity", Constants.Conversions.rpm2ms(Constants.Values.TANKDRIVE_WHEEL_RADIUS, Chassis.getInstance().getRightVelocity()/ 10.97));
-    // SmartDashboard.putNumber("left Angle", Math.toDegrees(leftSideAngle));
-    // SmartDashboard.putNumber("right Angle", Math.toDegrees(rightSideAngle));
-    // SmartDashboard.putNumber("leftSideDistance", m_leftSideDistance);
-    // SmartDashboard.putNumber("rightSideDistance", m_rightSideDistance);
-    // SmartDashboard.putNumber("test", Math.sqrt(leftSideX * leftSideX  + leftSideY * leftSideY));
-    // SmartDashboard.putNumber("test2", Math.sqrt(rightSideX * rightSideX + rightSideY * rightSideX));  
-    m_time = System.currentTimeMillis();
-  
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
